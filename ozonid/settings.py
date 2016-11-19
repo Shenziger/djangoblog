c0afe8ff2a8dfdd8cc3cb521dc030e6f061eac2d
Larson <anton.larionov@gmail.com>
2016-11-19 10:15:38 -0500

    add model and view to ozonid

--- ozonid/settings.py
+++ ozonid/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ozonid2'
+    'ozonid'
 )
 
 MIDDLEWARE_CLASSES = (
--- ozonid/urls.py
+++ ozonid/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from ozonid.views import hello, main
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'ozonid.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url('^$', main),
+    url('^hello/$', hello),
     url(r'^admin/', include(admin.site.urls)),
 ]
